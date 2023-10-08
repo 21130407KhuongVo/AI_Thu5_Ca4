116fad20e874e36ad982aace4a34ae026925e123
khuongvo <khuongvo2105@gmail.com>
2023-10-08 10:10:09 +0700

    update execute of DFS graph search

---
+++ Workspace/Buoi2_Basic/src/k21/DFSGraphSearchAlgo.java
@@ -0,0 +1,74 @@
+package k21;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DFSGraphSearchAlgo implements ISearchAlgo{
+
+	/**
+	 * for graph search
+	 */
+	@Override
+	public Node execute(Node root, String goal) {
+		Stack<Node>frontier = new Stack<Node>();
+		frontier.add(root);
+		
+		List<Node>explored = new ArrayList<Node>();
+		while (!frontier.isEmpty()) {
+			Node current = frontier.peek();
+			if (current.getLabel().equals(goal)) {
+				return current;
+			} else {
+				explored.add(current);
+				List<Node> children = current.getChildrenNodes();
+				for (Node child : children) {
+					if (!frontier.contains(child) && !explored.contains(child)) {
+						frontier.add(child);
+						child.setParent(current);
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	/**
+	 * for graph search
+	 */
+	@Override
+	public Node execute(Node root, String start, String goal) {
+		Stack<Node>frontier = new Stack<Node>();
+		frontier.add(root);
+		
+		boolean visited = false;
+		
+		List<Node>explored = new ArrayList<Node>();
+		while (!frontier.isEmpty()) {
+			Node current = frontier.pop();
+			if (current.getLabel().equals(goal) && visited) {
+				return current;
+			} else {
+				if (current.getLabel().equals(start)) {
+					visited = true;
+					frontier.clear();
+					explored.clear();
+					current.setParent(null);
+					return execute(current, goal);
+				}
+				explored.add(current);
+				List<Node> children = current.getChildrenNodes();
+				for (Node child : children) {
+					if (!frontier.contains(child) && !explored.contains(child)) {
+						frontier.add(child);
+						child.setParent(current);
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+}
--- Workspace/Buoi2_Basic/src/k21/Test.java
+++ Workspace/Buoi2_Basic/src/k21/Test.java
@@ -11,6 +11,7 @@ public class Test {
 	static Node nodeG = new Node("G");
 	static Node nodeH = new Node("H");
 
+	//BFS for graph search
 	private static void testExecuteFromBFSGraphSearch() {
 		nodeS.addEdge(nodeA, 5);
 		nodeS.addEdge(nodeB, 2);
@@ -27,6 +28,7 @@ public class Test {
 		System.out.println(NodeUtils.printPath(result));
 	}
 	
+	//BFS (3 parameters) for graph search
 	private static void testExecuteFromBFSGraphSearch_3param() {
 		nodeS.addEdge(nodeA, 5);
 		nodeS.addEdge(nodeB, 2);
@@ -43,8 +45,42 @@ public class Test {
 		System.out.println(NodeUtils.printPath(result));
 	}
 
+	private static void testExecuteFromDFSGraphSearch() {
+		nodeS.addEdge(nodeA, 5);
+		nodeS.addEdge(nodeB, 2);
+		nodeS.addEdge(nodeC, 4);
+		nodeA.addEdge(nodeD, 9);
+		nodeA.addEdge(nodeE, 4);
+		nodeB.addEdge(nodeG, 6);
+		nodeC.addEdge(nodeF, 2);
+		nodeD.addEdge(nodeH, 7);
+		nodeE.addEdge(nodeG, 6);
+		nodeF.addEdge(nodeG, 1);
+		ISearchAlgo algol = new DFSGraphSearchAlgo();
+		Node result = algol.execute(nodeS, "G");
+		System.out.println(NodeUtils.printPath(result));
+	}
+	
+	private static void testExecuteFromDFSGraphSearch_3param() {
+		nodeS.addEdge(nodeA, 5);
+		nodeS.addEdge(nodeB, 2);
+		nodeS.addEdge(nodeC, 4);
+		nodeA.addEdge(nodeD, 9);
+		nodeA.addEdge(nodeE, 4);
+		nodeB.addEdge(nodeG, 6);
+		nodeC.addEdge(nodeF, 2);
+		nodeD.addEdge(nodeH, 7);
+		nodeE.addEdge(nodeG, 6);
+		nodeF.addEdge(nodeG, 1);
+		ISearchAlgo algol = new DFSGraphSearchAlgo();
+		Node result = algol.execute(nodeS, "A", "G");
+		System.out.println(NodeUtils.printPath(result));
+	}
+	
 	public static void main(String[] args) {
 //		testExecuteFromBFSGraphSearch();
-		testExecuteFromBFSGraphSearch_3param();
+//		testExecuteFromBFSGraphSearch_3param();
+//		testExecuteFromDFSGraphSearch();
+//		testExecuteFromDFSGraphSearch_3param();
 	}
 }
